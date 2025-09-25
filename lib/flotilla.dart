e3302c81590cbb38ef715ea5ad0ede304bba230d
gael <GaelP4585@gmail.com>
2025-09-25 14:35:45 -0700

    hay que seguirle

--- lib/flotilla.dart
+++ lib/flotilla.dart
@@ -1,59 +1,122 @@
 import 'package:dart_application_1/elemento.dart';
 import 'package:dart_application_1/punto.dart';
 
-enum TiposBarcos {bote, lancha ,submarino ,crucero, portaaviones}
-enum DireccionesHacia {arriba, abajo, izquierda, derecha}
+enum TiposBarcos { bote, lancha, submarino, crucero, portaaviones }
+enum DireccionesHacia { arriba, abajo, izquierda, derecha }
 
 class Barco {
   final TiposBarcos tipo;
   final DireccionesHacia direccion;
   final Punto puntoInicial;
   List<Elemento> elementos = [];
+
   Barco({
     required this.tipo,
     required this.direccion,
-    required this.puntoInicial
+    required this.puntoInicial,
   });
-}
 
+  Barco agregarBarco(Punto punto, TiposBarcos tipo, DireccionesHacia direccion) {
+    int tamanio;
+    switch (tipo) {
+      case TiposBarcos.bote:
+        tamanio = 1;
+        break;
+      case TiposBarcos.lancha:
+        tamanio = 2;
+        break;
+      case TiposBarcos.submarino:
+        tamanio = 3;
+        break;
+      case TiposBarcos.crucero:
+        tamanio = 4;
+        break;
+      case TiposBarcos.portaaviones:
+        tamanio = 5;
+        break;
+    }
+
+    for (int i = 0; i < tamanio; i++) {
+      switch (direccion) {
+        case DireccionesHacia.arriba:
+          elementos.add(
+            Elemento(punto: Punto(columna: punto.columna, fila: punto.fila - i)),
+          );
+          break;
+        case DireccionesHacia.abajo:
+          elementos.add(
+            Elemento(punto: Punto(columna: punto.columna, fila: punto.fila + i)),
+          );
+          break;
+        case DireccionesHacia.izquierda:
+          elementos.add(
+            Elemento(punto: Punto(columna: punto.columna - i, fila: punto.fila)),
+          );
+          break;
+        case DireccionesHacia.derecha:
+          elementos.add(
+            Elemento(punto: Punto(columna: punto.columna + i, fila: punto.fila)),
+          );
+          break;
+      }
+    }
+
+    return this;
+  }
+}
 
 class Flotilla {
-  List<Barco> _barcos;
+  final List<Barco> _barcos;
   int get cantidad => _barcos.length;
-  Flotilla(this._barcos){ 
-  if(!esCantidadCorrecta()) throw FlotillaCantidadException();
-  if(!sontiposCorrectos()) throw FlotillaTiposException();
-  if(!EstanPosicionAdecuada()) throw FlotillaPosicionAdecuadaException;
+
+  Flotilla(this._barcos) {
+    if (!esCantidadCorrecta()) throw FlotillaCantidadException();
+    if (!sonTiposCorrectos()) throw FlotillaTiposException();
+    if (!estanPosicionAdecuada(_barcos)) throw FlotillaPosicionAdecuadaException();
+    if (!estanSeparados(_barcos)) throw FlotillaPegadosException();
   }
-  
 
-bool EstanPosicionAdecuada(List<Barco> barcos){
-    return false;
+  bool estanPosicionAdecuada(List<Barco> barcos) {
+    return true;
+  }
+
+  bool esCantidadCorrecta() {
+    return _barcos.length == 5;
+  }
+
+  bool sonTiposCorrectos() {
+    return _barcos.map((b) => b.tipo).toSet().length == 5;
+  }
+
+  bool estanSeparados(List<Barco> barcos) {
+  final puntos = barcos.expand((b) => b.elementos.map((e) => e.punto)).toList();
+
+  for (int i = 0; i < puntos.length; i++) {
+    for (int j = 0; j < puntos.length; j++) {
+      if (i == j) continue; 
+
+      final p1 = puntos[i];
+      final p2 = puntos[j];
+
+      final filaDiff = (p1.fila - p2.fila).abs();
+      final colDiff = (p1.columna - p2.columna).abs();
+
+      if (filaDiff <= 1 && colDiff <= 1) {
+        return false; 
+      }
+    }
+  }
+
+  return true;
+}
 }
 
-bool esCantidadCorrecta(){
-}
 
-bool sontiposCorrectos(){
-}
+class FlotillaCantidadException extends Error {}
 
+class FlotillaTiposException extends Error {}
 
-  /*Flotilla(List<Barco>  this._barcos) {
-    if(_barcos.length != 5) throw FlotillaCantidadException();
-    if(_barcos.map((e) => e.tipo).toSet().length != 5) throw FlotillaTipos();
+class FlotillaPosicionAdecuadaException extends Error {}
 
-
-  }*/
-}
-
-class FlotillaCantidadException extends Error {
-
-}
-class FlotillaTiposException extends Error {
-
-}
-
-class FlotillaPosicionAdecuadaException extends Error {
-
-}
+class FlotillaPegadosException extends Error {}
 
--- test/flotillla_test.dart
+++ test/flotillla_test.dart
@@ -1,12 +1,79 @@
 import 'package:test/test.dart';
-import 'dart:math';
 import 'package:dart_application_1/flotilla.dart';
-
+import 'package:dart_application_1/punto.dart';
 
 void main() {
-  test('creaste un barco', (){
-    Flotilla flotilla = Flotilla([Barco()]);
-    expect(flotilla.cantidad, equals(1));
-    expect((x) => Flotilla([Barco(TiposBarcos.values, DireccionesHacia.abajo, puntoInicial: ),Barco(TiposBarcos.values DireccionesHacia.values, Punto)]), throwsA(isA<FlotillaCantidadException>()));
+  test('Debe crear flotilla correctamente con 5 barcos de distintos tipos', () {
+    final flotilla = Flotilla([
+      Barco(
+        tipo: TiposBarcos.bote,
+        direccion: DireccionesHacia.arriba,
+        puntoInicial: Punto(columna: 0, fila: 0),
+      ),
+      Barco(
+        tipo: TiposBarcos.lancha,
+        direccion: DireccionesHacia.abajo,
+        puntoInicial: Punto(columna: 1, fila: 0),
+      ),
+      Barco(
+        tipo: TiposBarcos.submarino,
+        direccion: DireccionesHacia.izquierda,
+        puntoInicial: Punto(columna: 2, fila: 0),
+      ),
+      Barco(
+        tipo: TiposBarcos.crucero,
+        direccion: DireccionesHacia.derecha,
+        puntoInicial: Punto(columna: 3, fila: 0),
+      ),
+      Barco(
+        tipo: TiposBarcos.portaaviones,
+        direccion: DireccionesHacia.arriba,
+        puntoInicial: Punto(columna: 4, fila: 0),
+      ),
+    ]);
+
+    expect(flotilla.cantidad, equals(5));
   });
+
+  test('Lancha de tamaño 2 hacia la derecha desde (1,1)', () {
+    var barco = Barco(
+      tipo: TiposBarcos.lancha,
+      direccion: DireccionesHacia.derecha,
+      puntoInicial: Punto(columna: 1, fila: 1),
+    );
+
+    var nuevoBarco = barco.agregarBarco(
+      Punto(columna: 1, fila: 1),
+      TiposBarcos.lancha,
+      DireccionesHacia.derecha,
+    );
+
+    expect(nuevoBarco.elementos.length, 2);
+
+    expect(nuevoBarco.elementos[0].punto.columna, 1);
+    expect(nuevoBarco.elementos[0].punto.fila, 1);
+
+    expect(nuevoBarco.elementos[1].punto.columna, 2);
+    expect(nuevoBarco.elementos[1].punto.fila, 1);
+  });
+
+  test('no pueden estar pegados los barcos', () {
+  expect(
+    () => Flotilla([
+      Barco(
+        tipo: TiposBarcos.bote,
+        direccion: DireccionesHacia.abajo,
+        puntoInicial: Punto(columna: 1, fila: 1),
+      )..agregarBarco(Punto(columna: 1, fila: 1), TiposBarcos.bote, DireccionesHacia.abajo),
+      Barco(
+        tipo: TiposBarcos.lancha,
+        direccion: DireccionesHacia.abajo,
+        puntoInicial: Punto(columna: 1, fila: 2),
+      )..agregarBarco(Punto(columna: 1, fila: 2), TiposBarcos.lancha, DireccionesHacia.abajo),
+    ]),
+    throwsA(isA<FlotillaPegadosException>()),
+  );
+});
+
+
 }
